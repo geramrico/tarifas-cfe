5d0133701fabb305e6fcf273bde5d425c9698dc2
Gerardo Morales <geramoralesrico@gmail.com>
2021-07-10 19:25:12 -0500

    corrected TOTAL from calculated cfe

--- script.py
+++ script.py
@@ -227,6 +227,6 @@ if has_ran:
         st.write('Cargo por Capacidad: ',capacity_charge, 'MXN')
         st.write('Cargo por Distribución: ',distribution_charge, 'MXN')
         st.write('Cargo Fijo Mensual: ',tariffs[5], 'MXN')
-        st.write('TOTAL: ',tariffs[5], 'MXN')
+        st.write('TOTAL: ',energy_charge+capacity_charge+distribution_charge+tariffs[5], 'MXN')
 
 
